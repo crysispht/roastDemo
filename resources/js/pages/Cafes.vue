d106fe0c6ae000b1c9cb36e42ccfac476afb1ea2
zhuhua <zhuhua@huanqiu.com>
2018-10-19 17:20:34 +0800

    init

---
+++ resources/js/pages/Cafes.vue
@@ -0,0 +1,27 @@
+<style>
+
+</style>
+
+<template>
+    <div>
+        <div class="grid-x">
+            <div class="large-9 medium-9 small-12 cell">
+                <cafe-map></cafe-map>
+            </div>
+            <div class="large-3 medium-3 small-12 cell">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import CafeMap from '../components/cafes/CafeMap'
+
+    export default {
+        components: {
+            CafeMap
+        }
+    }
+</script>
